c2df24d9a97fd641d367d018ef0ddb65b8cb4b23
mandarincha <157801116+mandarincha@users.noreply.github.com>
2024-01-28 11:30:52 +0500

    Update schedule.go

--- internal/domain/schedule.go
+++ internal/domain/schedule.go
@@ -5,24 +5,24 @@ type Schedule struct{
 	Id string `json:"id"`
 	UserId string 
 	Name string `json:"name"`
-	PhoneNumber string `json:"phone-number"`
+	PhoneNumber string `json:"phone_number"`
 	Date string `json:"date"`
-	FromTime string `json:"from-time"`
-	ToTime string `json:"to-time"`
+	FromTime string `json:"from_time"`
+	ToTime string `json:"to_time"`
 	Purpose string `json:"purpose"`
 	Agree string `json:"agree"`
-	IsAgree bool `json:"is-agree"`
-	DoctorID string `json:"doctor-id"`
+	IsAgree bool `json:"is_agree"`
+	DoctorID string `json:"doctor_id"`
 	Overview string 
 }
 type ScheduleResponse struct{
 	Id string `json:"id"`
 	Name string `json:"name"`
-	PhoneNumber string `json:"phone-number"`
+	PhoneNumber string `json:"phone_number"`
 	Date string `json:"date"`
-	FromTime string `json:"from-time"`
-	ToTime string `json:"to-time"`
+	FromTime string `json:"from_time"`
+	ToTime string `json:"to_time"`
 	Purpose string `json:"purpose"`
-	IsAgree bool `json:"is-agree"`
+	IsAgree bool `json:"is_agree"`
 	Doctor Doctor
 }
